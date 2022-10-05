6e82b00ad34e22050c1138f8ef21ab88c62f16ab
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-10-05 13:08:06 -0400

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Interview Bot
+
+## Setting up
+
+Follow this tutorial for installing [yarn](https://www.digitalocean.com/community/tutorials/how-to-install-and-use-the-yarn-package-manager-for-node-js).
+
+# TODO: Fresh clone and set up instructions...
---
+++ app.js
@@ -0,0 +1,39 @@
+const { App } = require("@slack/bolt");
+const { WebClient } = require('@slack/web-api');
+require("dotenv").config();
+
+const web = new WebClient(process.env.BOT_TOKEN);
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+
+
+app.command("/test", async ({ command, ack, say }) => {
+    try {
+      await ack();
+      say("Robot bot bot test...");
+    } catch (error) {
+        console.log("err")
+      console.error(error);
+    }
+});
+
+app.message(/hey/, async ({ command, say }) => {
+    try {
+      say("You said hey, hey is for horses...");
+    } catch (error) {
+        console.log("err")
+      console.error(error);
+    }
+});
+
+(async () => {
+  const port = 3000
+  // Start your app
+  await app.start(process.env.PORT || port);
+  console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
+})();
